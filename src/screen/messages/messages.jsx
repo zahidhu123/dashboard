9ad34b14c95749e923cde3ff6133e4d2e1ecce0a
unknown <zahidmd1996@gmail.com>
2022-09-07 02:36:35 +0530

    message page

--- src/screen/messages/messages.jsx
+++ src/screen/messages/messages.jsx
@@ -4,17 +4,33 @@ import Doctore from '../../assets/doctor.png'
 import Patient from '../../assets/patient.jpg'
 import Michael from '../../assets/michael-dam.jpg'
 import Metri from '../../assets/metri.jpg'
+import Oleg from '../../assets/oleg.jpg'
+import Albert from '../../assets/albert.jpg'
+import Charli from '../../assets/charli.jpg'
+import Teeth1 from '../../assets/teeth1.jpg'
+import Teeth2 from '../../assets/teeth2.jpg'
 
 export default function Messages() {
   return (
     <>
       <div className='m-box'>
-        <div className="row justify-content-between py-3">
-          <div className="col-lg-6">
-            <h3 className=''>Message Page</h3>
-          </div>
-          <div className="col-lg-6">
-            <p className='text-end'>Profile</p>
+        <div className='patient-nav1'>
+          <div className='d-flex justify-content-between align-items-center h-100'>
+            <div>
+              <p className='m-0 fs-22 fw-500 ps-4'><i class="fa-regular fa-comment-dots px-2 fs-24 profile-icon"></i>Message</p>
+            </div>
+            <div>
+              <div className='d-flex align-items-center'>
+                <div className='position-relative d-flex align-items-center'>
+                  <i className="fa-solid fa-magnifying-glass search-icon"></i>
+                  <input type="text" placeholder='search' className='rounded-pill search-input' />
+                </div>
+                <div className='add-btn'><i className="fa-regular fa-plus text-light fs-26 fw-700 "></i></div>
+                <div className='notifaction-btn'><i className="fa-solid fa-bell text-secondary"></i>
+                  <div className='red-dot'></div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="row">
@@ -24,10 +40,10 @@ export default function Messages() {
                 <i className="fa-solid fa-magnifying-glass search-icon"></i>
                 <input type="text" placeholder='search' className='rounded-pill search-input w-100' />
               </div>
-              <div className='row align-items-center py-3'>
+              <div className='row align-items-center pt-3 search-inp-box'>
                 <div className="col-10 p-0">
                   <div className='d-flex'>
-                    <img src={Doctore} alt="" className='small-img' />
+                    <img src={Albert} alt="" className='small-img' />
                     <div className='ps-2'>
                       <p className='title-name fw-bold m-0'>Diane Cooper</p>
                       <p className='title-subheading m-0'>Lorem ipsum dolor sit ame...</p>
@@ -43,7 +59,7 @@ export default function Messages() {
                   </div>
                 </div>
               </div>
-              <div className='row align-items-center py-3'>
+              <div className='row align-items-center pt-3 search-inp-box'>
                 <div className="col-10 p-0">
                   <div className='d-flex'>
                     <img src={Patient} alt="" className='small-img' />
@@ -62,10 +78,10 @@ export default function Messages() {
                   </div>
                 </div>
               </div>
-              <div className='row align-items-center py-3'>
+              <div className='row align-items-center pt-3 search-inp-box'>
                 <div className="col-10 p-0">
                   <div className='d-flex'>
-                    <img src={Doctore} alt="" className='small-img' />
+                    <img src={Metri} alt="" className='small-img' />
                     <div className='ps-2'>
                       <p className='title-name fw-bold m-0'>Diane Cooper</p>
                       <p className='title-subheading m-0'>Lorem ipsum dolor sit ame...</p>
@@ -81,13 +97,13 @@ export default function Messages() {
                   </div>
                 </div>
               </div>
-              <div className='row align-items-center py-3'>
+              <div className='row align-items-center pt-3 search-inp-box'>
                 <div className="col-10 p-0">
                   <div className='d-flex'>
-                    <img src={Doctore} alt="" className='small-img' />
+                    <img src={Michael} alt="" className='small-img' />
                     <div className='ps-2'>
-                      <p className='title-name fw-bold m-0'>Diane Cooper</p>
-                      <p className='title-subheading m-0'>Lorem ipsum dolor sit ame...</p>
+                      <p className='title-name m-0'>Diane Cooper</p>
+                      <p className='title-subheading1 m-0'>Lorem ipsum dolor sit ame...</p>
                     </div>
                   </div>
                 </div>
@@ -97,13 +113,13 @@ export default function Messages() {
                   </div>
                 </div>
               </div>
-              <div className='row align-items-center py-3'>
+              <div className='row align-items-center pt-3 search-inp-box'>
                 <div className="col-10 p-0">
                   <div className='d-flex'>
-                    <img src={Doctore} alt="" className='small-img' />
+                    <img src={Oleg} alt="" className='small-img' />
                     <div className='ps-2'>
-                      <p className='title-name fw-bold m-0'>Diane Cooper</p>
-                      <p className='title-subheading m-0'>Lorem ipsum dolor sit ame...</p>
+                      <p className='title-name m-0'>Diane Cooper</p>
+                      <p className='title-subheading1 m-0'>Lorem ipsum dolor sit ame...</p>
                     </div>
                   </div>
                 </div>
@@ -113,11 +129,54 @@ export default function Messages() {
                   </div>
                 </div>
               </div>
-              
+
             </div>
           </div>
           <div className="col-lg-9">
-
+            <div className="row">
+              <div className="col-lg-9">
+                <p>zahid</p>
+              </div>
+              <div className="col-lg-3">
+                <div className='active-wrapper1'>
+                  <div className='position-relative'>
+                    <img src={Charli} className="profile-img" alt="" />
+                    <div className='fisher-wrapper p-4'>
+                      <p className='profile-name fw-bold m-0'>Aubrey Fisher</p>
+                      <p className='profile-time m-0'>Last seen today at 13.42</p>
+                    </div>
+                  </div>
+                  <div className='p-3'>
+                    <p className='contact-info'>Contact Info</p>
+                    <div className=''>
+                      <p className='email-title'>Email Address</p>
+                      <p className='email-address'>aubdrey.ahoy@gmail.com</p>
+                    </div>
+                    <div>
+                      <p className='email-title'>Phone Number</p>
+                      <p className='email-address'>(91)8882094848</p>
+                    </div>
+                    <div>
+                      <p className='email-title'>Street Address</p>
+                      <p className='email-address'>JI. Diponegoro No. 21</p>
+                    </div>
+                    <div>
+                      <p className='email-title'>City</p>
+                      <p className='email-address m-0'>Cilacap</p>
+                    </div>
+                  </div>
+                  <div className='attachment-wrapper'>
+                    <div className='p-3'>
+                      <p className='contact-info'>Attachment</p>
+                      <div>
+                        <img src={Teeth1} className="teeth me-2" alt="" />
+                        <img src={Teeth2} className="teeth" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
